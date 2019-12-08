eefdfa193bd23b35aea842ea35f2b9043c8b4ae9
Roman Lyapaev <XilentDem@gmail.com>
2019-12-08 14:33:05 +0300

    add script(header-slider)

--- js/main.js
+++ js/main.js
@@ -18,29 +18,30 @@
 
 $(function () {
 
-	$('.slider-reviews').slick({
+	$('.header-slider__slider').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		speed: 500,
+		speed: 900,
 		autoplay: true,
-		autoplaySpeed: 3000,
+		autoplaySpeed: 5000,
 		infinite: true,
-		swipe: false,
+		draggable: false,
         accessibility: 'boolean',
-		fade: true,
-		arrows: false,
-		dots: true,
-		dotsClass: 'slider-dots',
-		pauseOnDotsHover: true,
-		customPaging : function(slider, i) {
-		var thumb = $(slider.$slides[i]).data();
-		return '<button>'+0+(i+1)+'</button>';
-		},
+        prevArrow: '<i class="fas slide-btn slide-btn_left fa-angle-left"></i>',
+        nextArrow: '<i class="fas slide-btn slide-btn_right fa-angle-right"></i>',
+        asNavFor: '.header-slider__slider-menu',
+		// fade: true,
+		// dots: true,
+		// dotsClass: 'slider-dots',
+		// pauseOnDotsHover: true,
+		// customPaging : function(slider, i) {
+		// var thumb = $(slider.$slides[i]).data();
+		// return '<button>'+0+(i+1)+'</button>';
+		// },
 		responsive: [
 			{
                 breakpoint: 992,
                 settings: {
-                    swipe: true
                 }
             },
             {
@@ -53,12 +54,20 @@ $(function () {
         ]
 	});
 
-	$('.question-block').click(function () {
-		$('.question-block').removeClass('active');
-		$(this).addClass('active');
-	});
+	$('.header-slider__slider-menu').slick({ // настройка навигации
+        slidesToShow: 5, // указываем что нужно показывать 3 навигационных изображения
+        asNavFor: '.header-slider__slider', // указываем что это навигация для блока выше
+        centerMode: false,
+        accessibility: 'boolean',
+        focusOnSelect: true // указываем что бы слайделось по клику
+    });
 
-	$('.btn-mobile-menu').click(function () {
-		$('.mobile-menu-block').toggleClass('active');
-	});
+	// $('.question-block').click(function () {
+	// 	$('.question-block').removeClass('active');
+	// 	$(this).addClass('active');
+	// });
+
+	// $('.btn-mobile-menu').click(function () {
+	// 	$('.mobile-menu-block').toggleClass('active');
+	// });
 });
